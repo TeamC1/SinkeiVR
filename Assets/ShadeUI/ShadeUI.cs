f259205bfde7b2e92b6a324ce8c981009db615fb
YujiTakebuchi <takeyou.swift@gmail.com>
2017-10-28 23:28:34 +0900

    add shade ui logic

---
+++ Assets/ShadeUI/ShadeUI.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadeUI : MonoBehaviour
+{
+	public GameObject Stick;
+	public float DisOfCam;
+	public float UIShadeSpeed;
+	private Vector3 DisOfUIAndCam;
+	private bool IsOpenedUI = false;
+	private bool IsUIActive = false;
+
+	// Use this for initialization
+	void Start ()
+	{
+		gameObject.transform.position = Camera.main.transform.position + new Vector3 (5f, -1f, DisOfCam);
+		Vector3 CamPos = Camera.main.transform.position + new Vector3 (0f, -1f, DisOfCam);
+		DisOfUIAndCam = CamPos - gameObject.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKey(KeyCode.Space)) {
+			if (!IsUIActive) {
+				if (!IsOpenedUI) {
+					StartCoroutine ("OpenUI");
+				} else {
+					StartCoroutine ("CloseUI");
+				}
+			}
+		}
+	}
+
+	IEnumerator OpenUI ()
+	{
+		Rigidbody UIRigid = gameObject.GetComponent<Rigidbody> ();
+		UIRigid.velocity = DisOfUIAndCam / UIShadeSpeed;
+		IsUIActive = true;
+
+		yield return new WaitForSeconds (UIShadeSpeed);
+
+		IsUIActive = false;
+		UIRigid.velocity = new Vector3 (0f, 0f, 0f);
+		IsOpenedUI = true;
+	}
+
+	IEnumerator CloseUI ()
+	{
+		Rigidbody UIRigid = gameObject.GetComponent<Rigidbody> ();
+		UIRigid.velocity = -DisOfUIAndCam / UIShadeSpeed;
+		IsUIActive = true;
+
+		yield return new WaitForSeconds (UIShadeSpeed);
+
+		IsUIActive = false;
+		UIRigid.velocity = new Vector3 (0f, 0f, 0f);
+		IsOpenedUI = false;
+	}
+}
---
+++ Assets/ShadeUI/flick.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flick : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Rigidbody rigidBody = gameObject.GetComponent<Rigidbody> ();
+		rigidBody.velocity = new Vector3 (-3, 0, 0);
+		Destroy (gameObject, 0.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
